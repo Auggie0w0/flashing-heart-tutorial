1091fa905015d11dadf65223aa6a31671f105377
August <117049354+Auggie0w0@users.noreply.github.com>
2024-04-23 15:58:31 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+from microbit import *
+
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.clear_screen()
+    basic.pause(500)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function on_forever() {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.clearScreen()
+    basic.pause(500)
+})
